4e396daa2413cb0d4cc7e9ef30e91b254b7fd80e
thenexoraweb <thenexoraweb@gmail.com>
2024-12-30 10:15:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,293 @@
+<?php include('header.php'); ?>
+
+    <main>
+
+        <!-- slider Area Start-->
+        <div class="slider-area ">
+            <div class="slider-active">
+                <div class="single-slider  hero-overly slider-height d-flex align-items-center" data-background="assets/images/pharmaceutical-and-chemical-intermediates.webp">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-11">
+                                <div class="hero__caption">
+                                    <div class="hero-text1">
+                                        <span data-animation="fadeInUp" data-delay=".3s">we are dedicated to providing</span>
+                                    </div>
+                                    <h1 data-animation="fadeInUp" data-delay=".5s">high-quality</h1>
+                                    <div class="stock-text" data-animation="fadeInUp" data-delay=".8s">
+                                        <h2>pharmaceutical</h2>
+                                        <h2>pharmaceutical</h2>
+                                    </div>
+                                    <div class="hero-text2 mt-110" data-animation="fadeInUp" data-delay=".9s">
+                                       <span><a href="services.html">Our Services</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single-slider  hero-overly slider-height d-flex align-items-center" data-background="assets/images/global-healthcare.webp">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-11">
+                                <div class="hero__caption">
+                                    <div class="hero-text1">
+                                        <span data-animation="fadeInUp" data-delay=".3s">we are dedicated to providing</span>
+                                    </div>
+                                    <h1 data-animation="fadeInUp" data-delay=".5s">chemical </h1>
+                                    <div class="stock-text" data-animation="fadeInUp" data-delay=".8s">
+                                        <h2>intermediates</h2>
+                                        <h2>intermediates</h2>
+                                    </div>
+                                    <div class="hero-text2 mt-110" data-animation="fadeInUp" data-delay=".9s">
+                                        <span><a href="services.html">Our Products</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- slider Area End-->
+        
+        <!-- About Area Start -->
+        <section class="support-company-area fix pt-10">
+            <div class="support-wrapper align-items-end">
+                <div class="left-content">
+                    <!-- section tittle -->
+                    <div class="section-tittle section-tittle2 mb-55">
+                        <div class="front-text">
+                            <h2 class="">Welcome to RCIL</h2>
+                        </div>
+                        <span class="back-text">About Us</span>
+						<h3>- Riya Chem Intermediates Limited</h3>
+                    </div>
+                    <div class="support-caption">
+                        <!-- <p class="pera-top">Mollit anim laborum duis au dolor in voluptcate velit ess cillum dolore eu lore dsu quality mollit anim laborumuis au dolor in 
+						voluptate velit cillu.</p>-->
+                        <p>At RCIL, we are dedicated to providing high-quality pharmaceutical and chemical intermediates to support the ever-evolving global healthcare and 
+						industrial needs. With a commitment to excellence, innovation, and sustainability, we take pride in being a trusted partner to leading industries 
+						worldwide.</p>
+                        <a href="about.html" class="btn red-btn2">read more</a>
+                    </div>
+                </div>
+                <div class="right-content">
+                    <!-- img -->
+                    <div class="right-img">
+                        <img src="assets/images/healthcare-industry.webp" alt="Riya Chem Intermediates Limited">
+                    </div>
+                    <div class="support-img-cap text-center">
+                        <span>2014</span>
+                        <p>Since</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- About Area End -->
+		
+		<!-- Services Area Start -->
+        <div class="services-area1 section-padding30">
+            <div class="container">
+                <!-- section tittle -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-tittle mb-55">
+                            <div class="front-text">
+                                <h2 class="">Our Expertise</h2>
+                            </div>
+                            <span class="back-text">Expertise</span>
+							<p>Discover a world of excellence with RCIL—a name synonymous with trust and innovation.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                        <div class="single-service-cap mb-30">
+                            <div class="service-img">
+                                <img src="assets/images/chemical-pharmaceutical.webp" alt="">
+                            </div>
+                            <div class="service-cap">
+								<p>Comprehensive range of premium chemical and pharmaceutical intermediates.</p> 
+                            </div>
+                            <div class="service-icon">
+                                <img src="assets/img/icon/services_icon1.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                        <div class="single-service-cap mb-30">
+                            <div class="service-img">
+                                <img src="assets/images/top-notch-quality.webp" alt="">
+                            </div>
+                            <div class="service-cap">
+                                <p>Advanced manufacturing practices ensuring top-notch quality.</p>
+                            </div>
+                            <div class="service-icon">
+                                <img src="assets/img/icon/services_icon1.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6">
+                        <div class="single-service-cap mb-30">
+                            <div class="service-img">
+                                <img src="assets/images/reliability-and-satisfaction.webp" alt="">
+                            </div>
+                            <div class="service-cap">
+                                <p>Client-centric approach focused on reliability and satisfaction.</p>
+                            </div>
+                            <div class="service-icon">
+                                <img src="assets/img/icon/services_icon1.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Services Area End -->
+		
+		
+        <!-- contact with us Start -->
+        <section class="contact-with-area" data-background="assets/images/chemical-industry.webp">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-9 offset-xl-1 offset-lg-1">
+                        <div class="contact-us-caption">
+                            <div class="team-info mb-30 pt-45">
+                                <!-- Section Tittle -->
+                                <div class="section-tittle section-tittle4">
+                                    <div class="front-text">
+                                        <h2 class="">Lats talk with us</h2>
+                                    </div>
+                                    <span class="back-text">Lat`s chat</span>
+                                </div>
+                                <p>At RCIL - Riya Chem Intermediates Limited, we're committed to providing you with exceptional service and support. Whether you have inquiries 
+								about our products, need assistance with orders, or require technical support, we’re here to help.</p>
+                                <a href="#" class="white-btn">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- contact with us End-->
+        <!-- CountDown Area Start -->
+        <div class="count-area">
+            <div class="container">
+                <div class="count-wrapper count-bg" data-background="assets/img/gallery/section-bg3.jpg">
+                    <div class="row justify-content-center" >
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="count-clients">
+                                <div class="single-counter">
+                                    <div class="count-number">
+                                        <span class="counter">10</span>
+                                    </div>
+                                    <div class="count-text">
+                                        <p>Years of</p>
+                                        <h5>Experience</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="count-clients">
+                                <div class="single-counter">
+                                    <div class="count-number">
+                                        <span class="counter">13</span>
+                                    </div>
+                                    <div class="count-text">
+                                        <p>Our</p>
+                                        <h5>Products</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-6">
+                            <div class="count-clients">
+                                <div class="single-counter">
+                                    <div class="count-number">
+                                        <span class="counter">04</span>
+                                    </div>
+                                    <div class="count-text">
+                                        <p>Our</p>
+                                        <h5>Services</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- CountDown Area End -->
+        
+        <!-- Testimonial Start -->
+        <div class="testimonial-area t-bg testimonial-padding">
+            <div class="container ">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <!-- Section Tittle -->
+                        <div class="section-tittle section-tittle6 mb-50">
+                            <div class="front-text">
+                                <h2 class="">Testimonial</h2>
+                            </div>
+                            <span class="back-text">Feedback</span>
+                        </div>
+                    </div>
+                </div>
+               <div class="row">
+                    <div class="col-xl-10 col-lg-11 col-md-10 offset-xl-1">
+                        <div class="h1-testimonial-active">
+                            <!-- Single Testimonial -->
+                            <div class="single-testimonial">
+                                 <!-- Testimonial Content -->
+                                <div class="testimonial-caption ">
+                                    <div class="testimonial-top-cap">
+                                        <!-- SVG icon -->
+                                        <svg xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink"width="86px" height="63px">
+                                        <path fill-rule="evenodd"  stroke-width="1px" stroke="rgb(255, 95, 19)" fill-opacity="0" fill="rgb(0, 0, 0)"
+                                        d="M82.623,59.861 L48.661,59.861 L48.661,25.988 L59.982,3.406 L76.963,3.406 L65.642,25.988 L82.623,25.988 L82.623,59.861 ZM3.377,25.988 L14.698,3.406 L31.679,3.406 L20.358,25.988 L37.340,25.988 L37.340,59.861 L3.377,59.861 L3.377,25.988 Z"/>
+                                        </svg>
+                                        <p>Mollit anim laborum.Dvcuis aute iruxvfg dhjkolohr in re voluptate velit esscillumlore eu quife nrulla parihatur. Excghcepteur sfwsignjnt occa cupidatat non aute iruxvfg dhjinulpadeserunt mollitemnth incididbnt ut;o5tu layjobore mofllit anim. Mollit anim laborum.Dvcuis aute iruxvfg dhjkolohr in re voluptate velit esscillumlore eu quife nrulla parihatur. Excghcepteur sfwsignjn.</p>
+                                    </div>
+                                    <!-- founder -->
+                                    <div class="testimonial-founder d-flex align-items-center">
+                                       <div class="founder-text">
+                                            <span>Jessya Inn</span>
+                                            <p>Co Founder</p>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Single Testimonial -->
+                            <div class="single-testimonial">
+                                 <!-- Testimonial Content -->
+                                <div class="testimonial-caption ">
+                                    <div class="testimonial-top-cap">
+                                        <!-- SVG icon -->
+                                        <svg xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink"width="86px" height="63px">
+                                        <path fill-rule="evenodd"  stroke-width="1px" stroke="rgb(255, 95, 19)" fill-opacity="0" fill="rgb(0, 0, 0)"
+                                        d="M82.623,59.861 L48.661,59.861 L48.661,25.988 L59.982,3.406 L76.963,3.406 L65.642,25.988 L82.623,25.988 L82.623,59.861 ZM3.377,25.988 L14.698,3.406 L31.679,3.406 L20.358,25.988 L37.340,25.988 L37.340,59.861 L3.377,59.861 L3.377,25.988 Z"/>
+                                        </svg>
+                                        <p>Mollit anim laborum.Dvcuis aute iruxvfg dhjkolohr in re voluptate velit esscillumlore eu quife nrulla parihatur. Excghcepteur sfwsignjnt occa cupidatat non aute iruxvfg dhjinulpadeserunt mollitemnth incididbnt ut;o5tu layjobore mofllit anim. Mollit anim laborum.Dvcuis aute iruxvfg dhjkolohr in re voluptate velit esscillumlore eu quife nrulla parihatur. Excghcepteur sfwsignjn.</p>
+                                    </div>
+                                    <!-- founder -->
+                                    <div class="testimonial-founder d-flex align-items-center">
+                                       <div class="founder-text">
+                                            <span>Jessya Inn</span>
+                                            <p>Co Founder</p>
+                                       </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+        
+
+    </main>
+	
+<?php include('footer.php'); ?>
